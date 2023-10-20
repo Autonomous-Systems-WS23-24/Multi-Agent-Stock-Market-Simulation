a4abcab906b0be396e67b20ce4a5c6cadf978312
EliasDSAI <elias.88348@gmail.com>
2023-10-20 03:11:02 +0100

    Added Orderbook, Investor, Communication of Stockdata

---
+++ Old_Junkcode.py
@@ -0,0 +1,19 @@
+
+
+class Stockmarket(Agent):
+    class MyBehav(CyclicBehaviour):
+        async def on_start(self):
+            print("Starting Stock Market . . .")
+            self.df = stock_cue_calc(pd.read_csv('archive/Stocks/zoes.us.txt'))
+            self.counter = 0
+        async def run(self):
+            print("Day: {}".format(self.counter))
+            self.stockprize = self.df["Open"][self.counter]
+            print(self.stockprize)
+            self.counter += 1
+            await asyncio.sleep(1)
+
+    async def setup(self):
+        print("Agent starting . . .")
+        b = self.MyBehav()
+        self.add_behaviour(b)
