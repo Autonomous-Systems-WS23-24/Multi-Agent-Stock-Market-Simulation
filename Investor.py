b10062d93546dc92a475d2fe75ba89604fd39528
EliasDSAI <elias.88348@gmail.com>
2023-11-06 18:34:17 +0000

    added transaction matching function

--- Investor.py
+++ Investor.py
@@ -47,17 +47,7 @@ class Investor(Agent):
 
 
     def strategy(self, dataframe_stockdata):
-        # The Relative Strength Index is a momentum oscillator that measures the speed and change of price movements.
-        # It ranges from 0 to 100 and is typically used to identify overbought or oversold conditions
-        RSI_value = dataframe_stockdata.at[dataframe_stockdata.index[-1], 'RSI']
-        price_low = dataframe_stockdata.at[dataframe_stockdata.index[-1], 'Low']
-        price_high = dataframe_stockdata.at[dataframe_stockdata.index[-1], 'High']
-        price_mean = (price_low + price_high) / 2
-        # moving average of last 52 days
-        MA52 = dataframe_stockdata.at[dataframe_stockdata.index[-1], '52-day MA']
-        #print(f'RSI: {RSI_value}', f'MA of alst 52 days: {MA52}')
-        # buying when RSI value is lower than 35, and the mean price is 5 euro lower than the MA52. Buy the stock for the mean price
-        buy_price, sell_price = strategies.strategy_name()
+        buy_price, sell_price = strategies.strategy_name(dataframe_stockdata)
         #print(f'{self.jid} wants to sell for {self.sell_price} and buy for {self.buy_price}')
         orderbook_entry = {
             "name": [self.jid[0]],
