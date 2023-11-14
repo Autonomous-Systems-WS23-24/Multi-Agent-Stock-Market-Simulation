d3cb430425a037d0efb2fb201f15666d85279fdd
Diederik60 <diederikdekker@hotmail.nl>
2023-11-14 18:25:11 +0000

    Update Broker.py

--- Broker.py
+++ Broker.py
@@ -18,54 +18,55 @@ class Broker(Agent):
         super().__init__(jid, password)
         self.num_investors = num_investors
         self.num_iterations = num_iterations
-        class BrokerBehaviour(CyclicBehaviour):
-            async def on_start(self):
-                print(f"Starting Broker. . .")
-                self.count = 0
-            async def run(self):
-                self.investor_list = [f"investor{i}" for i in range(1, self.agent.num_investors + 1)]
 
-                #end condition
-                self.count += 1
-                if self.count == self.agent.num_iterations:
-                    self.kill()
+    class BrokerBehaviour(CyclicBehaviour):
+        async def on_start(self):
+            print(f"Starting Broker. . .")
+            self.count = 0
 
+        async def run(self):
+            self.investor_list = [f"investor{i}" for i in range(1, self.agent.num_investors + 1)]
 
+            # end condition
+            self.count += 1
+            if self.count == self.agent.num_iterations:
+                self.kill()
 
-            async def receive_offers(self):
-                for i in range(len(self.investor_list)):
-                    offers = await self.receive(timeout=10)  # wait for a message for 10 seconds
-                    if offers:
-                        print(f"Offers from Agent {offers.sender} received!")
-                        print(offers.body)
-                        with warnings.catch_warnings():
-                            warnings.filterwarnings("ignore", category=FutureWarning)
-                            df_offer = pd.read_json(offers.body, orient="split")
-                            #Code for adding buy offer to environment
-                            if df_offer['sell'] == np.nan:
-                                stock = ''
-                                price = df_offer.loc[0, 'buy']
-                                quantity = df_offer.loc[0, 'quantity']
-                                investor_name = offers.sender
-                                self.agent.environment.put_buy_offer(stock ,price, quantity, investor_name)
+        async def receive_offers(self):
+            for i in range(len(self.investor_list)):
+                offers = await self.receive(timeout=10)  # wait for a message for 10 seconds
+                if offers:
+                    print(f"Offers from Agent {offers.sender} received!")
+                    print(offers.body)
+                    with warnings.catch_warnings():
+                        warnings.filterwarnings("ignore", category=FutureWarning)
+                        df_offer = pd.read_json(offers.body, orient="split")
+                        # Code for adding buy offer to environment
+                        if df_offer['sell'] == np.nan:
+                            stock = ''
+                            price = df_offer.loc[0, 'buy']
+                            quantity = df_offer.loc[0, 'quantity']
+                            investor_name = offers.sender
+                            self.agent.environment.put_buy_offer(stock, price, quantity, investor_name)
 
-                            if df_offer['buy'] == np.nan:
-                                stock = ''
-                                price = df_offer.loc[0,'sell']
-                                quantity = df_offer.loc[0, 'quantity']
-                                investor_name = offers.sender
-                                self.agent.environment.put_sell_offer(stock ,price, quantity, investor_name)
+                        if df_offer['buy'] == np.nan:
+                            stock = ''
+                            price = df_offer.loc[0, 'sell']
+                            quantity = df_offer.loc[0, 'quantity']
+                            investor_name = offers.sender
+                            self.agent.environment.put_sell_offer(stock, price, quantity, investor_name)
 
-                            if df_offer['buy'] == np.nan and df_offer['sell'] == np.nan:
-                                pass
-                    else:
-                        print("Broker did not receive any stockdata after 10 seconds")
+                        if df_offer['buy'] == np.nan and df_offer['sell'] == np.nan:
+                            pass
+                else:
+                    print("Broker did not receive any stockdata after 10 seconds")
 
-            async def on_end(self):
-                pass
+        async def on_end(self):
+            pass
 
     async def setup(self):
         # Add the behavior to the agent
         template = Template()
         template.set_metadata("performative", "inform")
-        self.add_behaviour(BrokerBehaviour(), template)
+        b = BrokerBehaviour()
+        self.add_behaviour(b, template)
