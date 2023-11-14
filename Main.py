0ed72430c53242f3a7549933a60e74706ec98c6f
EliasDSAI <elias.88348@gmail.com>
2023-11-14 23:15:29 +0000

    update--

--- Main.py
+++ Main.py
@@ -6,14 +6,14 @@ import Orderbook
 import Investor
 import Environment
 import Broker
+import os
 
-async def main():
+async def main(stock_list):
     num_investors = 10
     num_iterations = 100
     risk_factors = np.arange(1,3.1,0.1)
     money_list = []
     stock_ownership_list = []
-    stock_list = ["zoes.us.txt"]
     environment = Environment.Environment(stock_list)
     Agent_Broker = Broker.Broker("broker@localhost", "1234",environment,num_investors,stock_list,num_iterations=num_iterations)
     investors = [Investor.Investor(f"investor{i}@localhost", "1234",environment,1,(i%4)*10,(i%5)*100,risk_factors[i],stock_list,num_iterations=num_iterations) for i in range(1, num_investors + 1)]
@@ -26,4 +26,7 @@ async def main():
     await wait_until_finished(Agent_Broker)
 
 if __name__ == "__main__":
-    spade.run(main())
+    package_dir = os.path.dirname(os.path.abspath(__file__))
+    data_directory = os.path.join(package_dir, 'archive/Stocks')
+    stock_list = os.listdir(data_directory)
+    spade.run(main(stock_list))
