9f3338687a1caeefb87e96af61c15728337a6919
EliasDSAI <elias.88348@gmail.com>
2023-11-21 01:14:20 +0000

    fix opinions

--- Investor.py
+++ Investor.py
@@ -5,7 +5,6 @@ from spade.agent import Agent
 from spade.behaviour import CyclicBehaviour
 from spade.template import Template
 from spade.message import Message
-import warnings
 import Strategies
 import json
 
--- Scenarios_Execution_Script.py
+++
@@ -1,8 +0,0 @@
-import Main
-
-
-
-
-if __name__ == "__main__":
-    for scenario in scenarios:
-        Main.main(scenario)
